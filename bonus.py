ba49d11409a9d05ca3de995c827feed7bcb57064
joleen tanous <116711100+joleentanous@users.noreply.github.com>
2024-03-13 00:49:22 +0200

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+
+
+def main():
+    iris = datasets.load_iris()
+    X = iris.data
+    inertias=[]
+    k_vals=[i for i in range(1, 11)]
+    for k in range(1,11):
+        kmeans = KMeans(n_clusters=k, init='k-means++', random_state=0)
+        kmeans.fit(X)
+        inertia = kmeans.inertia_
+        inertias.append(inertia)
+    plt.plot(k_vals,inertias)
+    plt.xlabel('K')
+    plt.ylabel('Average Dispersion')
+    plt.legend('Elbow Method for selection of optimal "K" clusters')
+    plt.show()
+
+main()
 
